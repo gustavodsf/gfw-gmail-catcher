3fe114e590461bd64f162e55462e0819a46dad1a
gustavodsf <gustavodsf1@gmail.com>
2022-04-24 14:01:41 -0300

    add config file

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
+import os
+import schedule
+import logging
+
+from src.manager import Manager
+from src.config import Config
+
 if __name__ == "__main__":
-    print("Hello World!")
+    config = Config()
+    configParams = config.read()
+    logging.basicConfig(filename='{}gfw-catcher.log'.format(os.getenv('WRITE_PATH')),
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        filemode='a',
+                        datefmt='%d/%m/%Y %I:%M:%S %p',
+                        level=logging.INFO)
+
+    # manager = Manager(configParams)
+
+    # schedule.every().day.at(configParams.get("exec_time")).do(manager.runCapture)
+
+    # while True:
+    #    schedule.run_pending()
